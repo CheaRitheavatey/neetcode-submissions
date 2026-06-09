6b9d042609e75360d5db8acfcc1440c56a44f599
CheaRitheavatey <137879197+CheaRitheavatey@users.noreply.github.com>
2026-06-09 21:10:32 +0700

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        longest = 0 
+        count = [0] * 26
+        for r in range(len(s)):
+            count[ord(s[r]) - 65] += 1
+            while (r - l + 1) - max(count) > k :
+                count[ord(s[l]) -65] -= 1
+                l+= 1
+            longest = max(longest, (r-l+1))
+        return longest
+        # AAABABB
+        # for i in range: ber s[i] doch knea continue -> add jol set 
+        # ber a new char we never see yg trov covert
+        # max()
+        
