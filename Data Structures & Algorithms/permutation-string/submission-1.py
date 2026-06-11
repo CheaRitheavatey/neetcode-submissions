20c4496ca712e654ab1a08ae13bdbc26afd84737
CheaRitheavatey <137879197+CheaRitheavatey@users.noreply.github.com>
2026-06-11 20:06:45 +0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,59 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # # abc
+        # # lecabee
+        # #   l
+        # #      r
+
+        # #      if length is the same return true
+
+        # # left move when we dont see it in s1 and r move everytime
+        # if len(s2) < len(s1): return False
+
+        # ss1 = [0] * 26
+        # ss2 = [0] * 26
+
+        # for i in range(len(s1)):
+        #     ss1[ord(s1[i]) - ord('a')]+=1
+        #     ss2[ord(s2[i]) - ord('a')]+=1
+
+        # if ss1 == ss2:
+        #     return True
+        
+        # l = 0
+        # ss = set()
+        # for r in range(len(s1), len(s2)):
+        #     ss2[ord(s2[r]) - ord('a')]+= 1
+        #     ss2[ord(s2[l]) - ord('a')]-= 1
+        #     l+=1
+            
+        #     if ss1 == ss2:
+        #         return True
+        # return False
+
+        # ss1 = [1,1,1,0,0,...]
+        # ss2 = [1,1,1,0,1,0,1,0,0,...]
+        store1 = [0] * 26
+        store2 = [0] * 26
+        if len(s2) < len(s1): return False
+        l = 0
+        for i in range(len(s1)):
+            store1[ord(s1[i]) - ord('a')] += 1
+            store2[ord(s2[i]) - ord('a')] += 1
+        if store1== store2: return True
+
+        for r in range(len(s1), len(s2)):
+            store2[ord(s2[r]) - ord('a')] += 1
+            store2[ord(s2[l]) - ord('a')] -= 1
+            l+= 1
+
+            if store1 == store2:
+                return True
+        return False
+            
+
+
+
+
+
+        
